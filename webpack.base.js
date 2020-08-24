5c0d2d34b91ede165276b369ef16246358ee2f18
liangliang <945904863@qq.com>
2020-08-25 03:17:21 +0800

    日常提交

--- webpack.base.js
+++ webpack.base.js
@@ -26,12 +26,44 @@ module.exports = {
                 use: [
                     miniCssExtractPlugin.loader,
                     'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: ()=>[
+                                require('postcss-preset-env')()
+                            ]
+                        }
+                    },
                     'sass-loader']
             },
             // 下载file-loaer, url-loader来处理图片和字体图标
             {
                 test: /\.(woff|woff2|eot|ttf|otf|png|svg|jpg|gif)$/, 
                 use: ['file-loader'] 
+            },
+            //下载eslint,eslint-loader
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                enforce: 'pre',
+                use: [
+                    {
+                        loader: 'eslint-loader',
+                        //优先执行该loader
+                        
+                    }
+                ]
+            },
+            //下载babel-loader、@babel/preset-env、@babel/core
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader:'babel-loader',   
+                    }
+                ]
             }
         ]
     },
