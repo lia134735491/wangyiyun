639286f2f7f7c338258af50d84bb250e982c0763
liangliang <945904863@qq.com>
2020-10-22 14:17:56 +0800

    日常提交

--- src/components/discoverchild/discoverchild.vue
+++ src/components/discoverchild/discoverchild.vue
@@ -79,6 +79,150 @@
               <span></span>
             </span>
           </div>
+          <div class="tuijian-content">
+            <ul>
+              <li>
+                <div>
+                  <img src="http://p2.music.126.net/tWrF4w6xCJiR7joQt4hYYA==/109951165401204896.jpg?param=140y140">
+                  <a title="[华语速爆新歌] 薛之谦高甜单曲与你遇见"></a>
+                  <p class="bottom">
+                    <span></span>
+                    <span class="bofangliang">111万</span>
+                    <a></a>
+                  </p>
+                </div>
+                <p class="bottom-title">
+                  <a>[华语速爆新歌] 薛之谦高甜单曲与你遇见</a>
+                </p>
+              </li>
+              <li>
+                <div>
+                  <img src="http://p2.music.126.net/tWrF4w6xCJiR7joQt4hYYA==/109951165401204896.jpg?param=140y140">
+                  <a title="[华语速爆新歌] 薛之谦高甜单曲与你遇见"></a>
+                  <p class="bottom">
+                    <span></span>
+                    <span class="bofangliang">111万</span>
+                    <a></a>
+                  </p>
+                </div>
+                <p class="bottom-title">
+                  <a>[华语速爆新歌] 薛之谦高甜单曲与你遇见</a>
+                </p>
+              </li><li>
+                <div>
+                  <img src="http://p2.music.126.net/tWrF4w6xCJiR7joQt4hYYA==/109951165401204896.jpg?param=140y140">
+                  <a title="[华语速爆新歌] 薛之谦高甜单曲与你遇见"></a>
+                  <p class="bottom">
+                    <span></span>
+                    <span class="bofangliang">111万</span>
+                    <a></a>
+                  </p>
+                </div>
+                <p class="bottom-title">
+                  <a>[华语速爆新歌] 薛之谦高甜单曲与你遇见</a>
+                </p>
+              </li><li>
+                <div>
+                  <img src="http://p2.music.126.net/tWrF4w6xCJiR7joQt4hYYA==/109951165401204896.jpg?param=140y140">
+                  <a title="[华语速爆新歌] 薛之谦高甜单曲与你遇见"></a>
+                  <p class="bottom">
+                    <span></span>
+                    <span class="bofangliang">111万</span>
+                    <a></a>
+                  </p>
+                </div>
+                <p class="bottom-title">
+                  <a>[华语速爆新歌] 薛之谦高甜单曲与你遇见</a>
+                </p>
+              </li><li>
+                <div>
+                  <img src="http://p2.music.126.net/tWrF4w6xCJiR7joQt4hYYA==/109951165401204896.jpg?param=140y140">
+                  <a title="[华语速爆新歌] 薛之谦高甜单曲与你遇见"></a>
+                  <p class="bottom">
+                    <span></span>
+                    <span class="bofangliang">111万</span>
+                    <a></a>
+                  </p>
+                </div>
+                <p class="bottom-title">
+                  <a>[华语速爆新歌] 薛之谦高甜单曲与你遇见</a>
+                </p>
+              </li><li>
+                <div>
+                  <img src="http://p2.music.126.net/tWrF4w6xCJiR7joQt4hYYA==/109951165401204896.jpg?param=140y140">
+                  <a title="[华语速爆新歌] 薛之谦高甜单曲与你遇见"></a>
+                  <p class="bottom">
+                    <span></span>
+                    <span class="bofangliang">111万</span>
+                    <a></a>
+                  </p>
+                </div>
+                <p class="bottom-title">
+                  <a>[华语速爆新歌] 薛之谦高甜单曲与你遇见</a>
+                </p>
+              </li><li>
+                <div>
+                  <img src="http://p2.music.126.net/tWrF4w6xCJiR7joQt4hYYA==/109951165401204896.jpg?param=140y140">
+                  <a title="[华语速爆新歌] 薛之谦高甜单曲与你遇见"></a>
+                  <p class="bottom">
+                    <span></span>
+                    <span class="bofangliang">111万</span>
+                    <a></a>
+                  </p>
+                </div>
+                <p class="bottom-title">
+                  <a>[华语速爆新歌] 薛之谦高甜单曲与你遇见</a>
+                </p>
+              </li><li>
+                <div>
+                  <img src="http://p2.music.126.net/tWrF4w6xCJiR7joQt4hYYA==/109951165401204896.jpg?param=140y140">
+                  <a title="[华语速爆新歌] 薛之谦高甜单曲与你遇见"></a>
+                  <p class="bottom">
+                    <span></span>
+                    <span class="bofangliang">111万</span>
+                    <a></a>
+                  </p>
+                </div>
+                <p class="bottom-title">
+                  <a>[华语速爆新歌] 薛之谦高甜单曲与你遇见</a>
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div class="space-white"></div>
+          <div class="tuijian">
+            <div class="tuijian-left">
+              <a href="javascript:;">新碟上架</a>
+            </div>
+            <span class="tuijian-more">
+              <a>更多</a>
+              <span></span>
+            </span>
+          </div>
+          <div class="xindie-content">
+            <a class="left-sanjiao">&nbsp;</a>
+            <div class="xindie-song">
+              <ul>
+                <li>
+                  <div>
+                    <img src="http://p3.music.126.net/q6h3zFrzr6Y3NlQT8rir6w==/109951165373635125.jpg?param=100y100">
+                    <a class="backi"></a>
+                  </div>
+                  <p>
+                    <a>Hybrid Theory (20th Anniversary Edition)</a>
+                  </p>
+                  <p>
+                    <a>Linkin Park</a>
+                  </p>
+                </li>
+                <li></li>
+                <li></li>
+                <li></li>
+
+              </ul>
+            </div>
+            <a class="right-sanjiao">&nbsp;</a>
+          </div>
         </div>
       </div>
       <div class="content-right"></div>
@@ -260,6 +404,139 @@ export default {
           margin-left: 4px;
           display: inline-block;
           background: url('../../images/index.png') 0 -240px;
+        }
+      }
+    }
+    .tuijian-content {
+      margin: 20px 0 0 -42px;
+      li {
+        display: inline-block;
+        padding: 0 0 30px 42px;
+        div {
+          width: 140px;
+          height: 140px;
+          position: relative;
+          img {
+            width: 100%;
+            height: 100%;
+          }
+          >a {
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+          }
+          .bottom {
+            width: 100%;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            height: 27px;
+            background: url('../../images/coverall.png') 0 -537px;
+            span:first-child {
+              float: left;
+              width: 14px;
+              height: 11px;
+              
+              margin: 9px 5px 9px 10px;
+              background: url('../../images/iconall.png');
+              background-position: 0 -24px;
+            }
+            .bofangliang {
+              float: left;
+              margin: 7px 0 0 0;
+              color: #ccc;
+              font-size: 12px;
+            }
+            a {
+              position: absolute;
+              right: 10px;
+              bottom: 5px;
+              width: 16px;
+              height: 17px;
+              background: url('../../images/iconall.png');
+              background-position: 0 0;
+              
+            }
+          }
+        }
+        .bottom-title {
+          margin: 8px 0 3px;
+          width: 140px;
+          font-size: 14px;
+        }
+      }
+    }
+    .space-white {
+      margin-bottom: 35px;
+    }
+    .xindie-content {
+      position: relative;
+      margin: 20px 0 37px;
+      border: 1px solid #d3d3d3;
+      background: #f5f5f5;
+      height: 186px;
+      padding-left: 16px;
+      .left-sanjiao {
+        position: absolute;
+        left: 4px;
+        background: url('../../images/index.png') -266px -76px;
+        top: 71px;
+        width: 17px;
+        height: 17px;
+      }
+      .right-sanjiao {
+        background: url('../../images/index.png') -306px -76px;
+        position: absolute;
+        right: 2px;
+        top: 71px;
+        width: 17px;
+        height: 17px;
+      }
+      .xindie-song {
+        width: 645px;
+        margin-top: 28px;
+        ul {
+          overflow: hidden;
+          white-space:nowrap;
+          li {
+            
+            display: inline-block;
+            margin-left: 11px;
+            width: 118px;
+            height: 150px;
+            // background: red;
+            p {
+              width: 90%;
+              font-size: 12px;
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+              a {
+                line-height: 18px;
+              }
+            }
+            div {
+              position: relative;
+              width: 100px;
+              height: 100px;
+              margin-bottom: 7px;
+              img {
+                display: block;
+                width: 100%;
+                height: 100%;
+              }
+              .backi {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 118px;
+                height: 100px;
+                background: url('../../images/coverall.png') 0 -570px;
+              }
+            }
+          }
         }
       }
     }
